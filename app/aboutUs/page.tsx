af78caf336f3fc253d7eae5278d1df38c497cf1e
damilola-awopegba <olabiyidamilola30@gmail.com>
2023-08-25 19:01:17 -0500

    first commit

---
+++ app/aboutUs/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from '@/Component/header'
+
+function page() {
+  return (
+    <div>
+        <Header />
+      <p>About us Page</p>
+      
+    </div>
+  )
+}
+
+export default page
